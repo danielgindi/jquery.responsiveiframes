fd12ed6170a1715fb5284e2c386044d637aed74b
Daniel Cohen Gindi <Danielgindi@gmail.com>
2016-05-10 12:09:38 +0300

    Minified latest version

--- jquery.responsiveiframes-min.js
+++ jquery.responsiveiframes-min.js
@@ -2,4 +2,4 @@
     Library by Daniel Cohen Gindi (danielgindi@gmail.com)
     MIT License!
 */
-(function(c){c(window).on("resize load",function(){c("iframe,embed").not(".no-aspect-ratio").each(function(){var a=c(this),b=parseFloat(a.css("width"))?a.width():Infinity,d=a.data("aspect-ratio");null==d&&(d=a.height()/a.width(),a.data("aspect-ratio",d).removeAttr("height").removeAttr("width").css({width:"",height:""}));b=Math.min(b,a.parent().width());a.width(b).height(b*a.data("aspect-ratio"))})})})(jQuery);
+(function(c){c(window).on("resize load",function(){c("iframe,embed").not(".no-aspect-ratio").each(function(){var a=c(this),b=a.data("aspect-ratio"),d=a.data("max-width");null==b&&(d=parseFloat(a.css("width"))?a.width():Infinity,b=a.height()/a.width(),a.data({"aspect-ratio":b,"max-width":d}).removeAttr("height").removeAttr("width").css({width:"",height:""}));b=Math.min(d,a.parent().width());a.width(b).height(b*a.data("aspect-ratio"))})})})(jQuery);
