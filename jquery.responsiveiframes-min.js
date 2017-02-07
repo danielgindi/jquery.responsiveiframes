1e8127ee1e23da8bbeeb9b969ae2080d0c7745db
Daniel Cohen Gindi <Danielgindi@gmail.com>
2017-02-07 18:18:04 +0200

    Update jquery.responsiveiframes-min.js

--- jquery.responsiveiframes-min.js
+++ jquery.responsiveiframes-min.js
@@ -2,5 +2,5 @@
  jquery.responsiveiframes
  git://github.com/danielgindi/jquery.responsiveiframes.git
 */
-function c(d){d(window).on("resize load",function(){d("iframe,embed").not(".no-aspect-ratio").each(function(){var a=d(this),b=a.data("aspect-ratio"),e=a.data("max-width");null==b&&(e=parseFloat(a.css("width"))?a.width():Infinity,b=a.height()/a.width(),a.data({"aspect-ratio":b,"max-width":e}).removeAttr("height").removeAttr("width").css({width:"",height:""}));b=Math.min(e,a.parent().width());a.width(b).height(b*a.data("aspect-ratio"))})})}
-"function"===typeof define&&define.a?define("jquery.responsiveiframes",["jquery"],c):"object"===typeof exports?module.b=c(require("jquery")):c(this.jQuery);
+(function(c,a){"function"===typeof define&&define.amd?define("jquery.responsiveiframes",["jquery"],a):"object"===typeof exports?module.exports=a(require("jquery")):a(c.jQuery)})(this,function(c){c(window).on("resize load",function(){c("iframe,embed").not(".no-aspect-ratio").each(function(){var a=c(this),b=a.data("aspect-ratio"),d=a.data("max-width");null==b&&(d=parseFloat(a.css("width"))?a.width():Infinity,b=a.height()/a.width(),a.data({"aspect-ratio":b,"max-width":d}).removeAttr("height").removeAttr("width").css({width:"",
+height:""}));b=Math.min(d,a.parent().width());a.width(b).height(b*a.data("aspect-ratio"))})})});
